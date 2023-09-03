4514a352c8b0f452bc61825ea10b6f2ec0dbe229
Andrei Gridin <andreigridin918@gmail.com>
2023-09-03 21:30:11 +0500

    Добавьте файлы проекта.

---
+++ Multiple/Multiple.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    // Изменение кодировки консоли:
+    system("chcp 1251 > nul");
+
+    int a = 0;
+    int b = 0;
+
+    cout << "Введите первое число: ";
+    cin >> a;
+    cout << "Введите второе число: ";
+    cin >> b;
+
+    if (b == 0) {
+        cout << "Деление на ноль не допустимо!" << endl;
+    }
+    else {
+        if (a % b == 0) {
+            cout << "Число " << a << " кратно числу " << b << "!" << endl;
+        }
+        else {
+            cout << "Число " << a << " не делится на число " << b << " без остатка!" << endl;
+        }
+    }
+
+    // Задержка консольного окна:
+    system("pause > nul");
+    return 0;
+}
